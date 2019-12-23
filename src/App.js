37ef71697af1d4939ab2f1aad1d8b404663e1eb2
Trevor254 <trevoromondi607@gmail.com>
2019-12-23 16:02:22 +0300

    modify files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Toolbar from './components/Toolbar/Toolbar';
 function App() {
   return (
     <div className="App">
-     
+     <Toolbar/>
     </div>
   );
 }
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import './Toolbar.css';
 
 const Toolbar = () => {
     return(
-        <header>
-            <nav>
+        <header className="toolbar">
+            <nav className="toolbar__navigation">
                 <div>
 
                 </div>
 
-                <div>
-
+                <div className="toolbar_logo">
+                  <p>THE LOGO</p>
                 </div>
 
-                <div>
+                <div className="spacer"/>
+
+                <div className="toolbar_navigation-items">
                    <ul>
                        <li><a href="/">Home</a></li>
                        <li><a href="/">About</a></li>
@@ -25,3 +28,5 @@ const Toolbar = () => {
         </header>
     )
 }
+
+export default Toolbar;
