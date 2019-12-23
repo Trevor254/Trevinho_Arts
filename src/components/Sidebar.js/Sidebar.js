577fce41b98b1fd1bcf47b6628ca1984580a26db
Trevor254 <trevoromondi607@gmail.com>
2019-12-23 16:08:23 +0300

    add new files

---
+++ src/components/Sidebar.js/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const toggleButton = props =>{
+   <div>
+       <button className="toggle-button">
+           <div className="toggle-button__line"/>
+           <div className="toggle-button__line"/>
+           <div className="toggle-button__line"/>
+       </button>
+   </div>
+}
+
+export default toggleButton;
