ec001f18b34d7553315a904651a79105a73dbaaa
Trevor254 <trevoromondi607@gmail.com>
2019-12-23 14:57:31 +0300

    delete some files,modify app.js

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Toolbar = () => {
+    return(
+        <header>
+            <nav>
+                <div>
+
+                </div>
+
+                <div>
+
+                </div>
+
+                <div>
+                   <ul>
+                       <li><a href="/">Home</a></li>
+                       <li><a href="/">About</a></li>
+                       <li><a href="/">Gallery</a></li>
+                       <li><a href="/">Cart</a></li>
+                       <li><a href="/">Contact</a></li>
+                   </ul>
+                </div>
+            </nav>
+        </header>
+    )
+}
