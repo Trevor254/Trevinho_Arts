a282e07c191585b0b7a0381ac17f1857ef7f0b31
Trevor254 <trevoromondi607@gmail.com>
2019-12-23 17:07:38 +0300

    modified files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Toolbar from './components/Toolbar/Toolbar';
+import Sidebar from './components/Sidebar/Sidebar';
 function App() {
   return (
     <div className="App">
      <Toolbar/>
+     <Sidebar/>
     </div>
   );
 }
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = props => {
+    return(
+       <nav className="side-drawer">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">About</a></li>
+          <li><a href="/">Gallery</a></li>
+          <li><a href="/">Cart</a></li>
+          <li><a href="/">Contact</a></li>
+        </ul>
+       </nav>
+    )
+}
+
+export default Sidebar;
