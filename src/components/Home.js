3af4c43d1af51ed42cc9540e65ea2f04f86d38ba
Trevor254 <trevoromondi607@gmail.com>
2019-12-31 12:07:50 +0300

    fixed styling issues

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,12 @@ const Home = () => {
         <div>
             <img className="title-logo" src={logo} alt=""/>
         </div>
-        <div className="text-container">
-	       <h3>Welcome To Trevinho Arts</h3>
-         <p className="paragraph-1">
-         The common pencil derived from the latin pronounciation ' peniculus ' meaning brush, 
+        <br/>
+        <h2 align="center" className="Main-title">Welcome To Trevinho Arts</h2>
+        <br/>
+        <br/>
+        <p align="center" className="text-1">
+        The common pencil derived from the latin pronounciation ' peniculus ' meaning brush, 
          used by draughtsmen around the world, is the most immediate and sensitive of the drawing 
          media being as capable of producing a quick sketch or a finely worked drawing. 
          Pencil drawing, drawing executed with an instrument composed of graphite enclosed in a 
@@ -19,18 +21,18 @@ const Home = () => {
          because of its usefulness in easily producing linear gray-black strokes, became the successor 
          of the older, metallic drawing stylus, with which late medieval and Renaissance artists and 
          tradesmen sketched or wrote on paper, parchment, or wood.
-         </p>
-         <br/>
-         <p className="paragraph-2">
-         My art work is based on the concept of pencil media and draws on various inspirations be it 
+        </p>
+        <br/>
+        <br/>
+        <p align="center" className="text-2">
+        My art work is based on the concept of pencil media and draws on various inspirations be it 
          famous people , landscapes, animals, fictional characters, cultural media, imagination from 
          the mind, i see it as a way to express or communicate my thoughts to the world.
-
-         </p>
-        </div>
+        </p>
       </div>
   
     )
 }
 
 export default Home;
+
