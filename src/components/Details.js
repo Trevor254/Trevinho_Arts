0758fc790a1c3e3f1e579bc2658505716cad914a
Trevor254 <trevoromondi607@gmail.com>
2020-01-15 11:24:11 +0300

    create sample function for test purposes

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import {ProductConsumer} from '../Context';
+import {Link} from 'react-router-dom';
+import {ButtonContainer} from './Button';
+
 
 export default class Details extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <ProductConsumer>
+                {(value)=> {
+                    console.log(value.detailImage);
+                }}
+            </ProductConsumer>
         )
     }
 }
