8cf9a1241ff6487bf163051214d1924b89135427
Trevor254 <trevoromondi607@gmail.com>
2020-01-18 17:12:42 +0300

    add new files

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Title from '../Title';
+import CartColumns from './CartColumns';
+
+
+class Cart extends React.Component {
+    render() {
+        return (
+            <section>
+                <Title name="your" title="cart"/>
+                <CartColumns/>
+            </section>
+        )
+    }
+}
+
+export default Cart;
