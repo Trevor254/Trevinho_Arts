8e7afb3fa321dd5608028719bc5cee18ee076fcf
Trevor254 <trevoromondi607@gmail.com>
2020-01-19 20:28:32 +0300

    modify cart page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import Title from '../Title';
 import CartColumns from './CartColumns';
-
+import EmptyCart from './EmptyCart';
+import {ProductConsumer} from '../../Context'
 
 class Cart extends React.Component {
     render() {
         return (
             <section>
-                <Title name="your" title="cart"/>
-                <CartColumns/>
+                <ProductConsumer>
+                    {value => {
+                        const {cart} = value;
+                        if(cart.length > 0){
+                            return(
+                               <div>
+                                  <Title name="your" title="cart"/>
+                                  <CartColumns/>
+                               </div>
+                            )
+                        } else {
+                          return <EmptyCart />
+                        }
+                    }}
+                </ProductConsumer>
+            
             </section>
         )
     }
